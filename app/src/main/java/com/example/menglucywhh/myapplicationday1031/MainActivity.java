0a319e7c2379ae9c9b700f653c68f02780473c39
Menglucywhh <962673186@qq.com>
2017-10-31 13:58:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/menglucywhh/myapplicationday1031/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.menglucywhh.myapplicationday1031;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
